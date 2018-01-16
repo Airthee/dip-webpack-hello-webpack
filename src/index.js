4ee5cba2fdb392169a8a3af71186c967988d86bc
Raphaël <raphael.tison@hotmail.fr>
2018-01-16 09:32:46 +0100

    Utilisation de babel

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 
-function component(){
-  var element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  return element;
-}
+// function component(){
+//   var element = document.createElement('div');
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   return element;
+// }
 
-function test(){
-  console.log(_.join(['test1', 'test2', 'rest3'], ' '));
-  console.log('Coucou');
-}
+// function test(){
+//   console.log(_.join(['test1', 'test2', 'rest3'], ' '));
+//   console.log('Coucou');
+// }
 
-document.body.appendChild(component());
-test();
+// document.body.appendChild(component());
+// test();
+
+const i = 5;
+const fonctionTest = ()=>{
+  console.log("Hello world !");
+};
+
+console.log(`Ceci est un test
+sur plusieurs lignes`);
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,18 @@ const path=require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: './bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
   }
-}
+};
